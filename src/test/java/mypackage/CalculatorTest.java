af7e92caab6e5a55fb5d3168007e4251704c5891
Beeranna Dude <101712802+beeru405@users.noreply.github.com>
2024-02-29 11:14:00 +0530

    Update CalculatorTest.java

--- src/test/java/mypackage/CalculatorTest.java
+++ src/test/java/mypackage/CalculatorTest.java
@@ -7,25 +7,22 @@ import static org.junit.Assert.assertThat;
 public class CalculatorTest {
     @Test
     public void twoAndThreeIsFive() throws Exception {
-        final long result = new Calculator().addFunc(2, 3);
+        final long result = new Calculator().addFucn(2, 3);
         assertThat(result, is(5L));
-        // Uncomment the following line to intentionally fail the test
-        // assertThat(result, is(6L));
+        //assertThat(result, is(6L));
     }
 
     @Test
     public void threeMinusTwoIsOne() throws Exception {
-        final long result = new Calculator().subFunc(2, 3);
+        final long result = new Calculator().subFucn(2, 3);
         assertThat(result, is(1L));
-        // Uncomment the following line to intentionally fail the test
         // assertThat(result, is(2L));
     }
-
+    
     @Test
     public void threeXThreeIsNine() throws Exception {
-        final long result = new Calculator().mulFunc(3, 3);
+        final long result = new Calculator().mulFucn(3, 3);
         assertThat(result, is(9L));
-        // Uncomment the following line to intentionally fail the test
-        // assertThat(result, is(8L));
+        //assertThat(result, is(8L));
     }
 }
