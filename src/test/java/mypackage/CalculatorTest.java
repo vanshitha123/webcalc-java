77ab0d9d851be34602a4dd86c473388746f5d47a
Beeranna Dude <101712802+beeru405@users.noreply.github.com>
2024-03-05 10:06:47 +0530

    Update CalculatorTest.java

--- src/test/java/mypackage/CalculatorTest.java
+++ src/test/java/mypackage/CalculatorTest.java
@@ -1,3 +1,4 @@
+/*
 package mypackage;
 
 import org.junit.Test;
@@ -24,5 +25,31 @@ public class CalculatorTest {
         final long result = new Calculator().mulFucn(3, 3);
         assertThat(result, is(9L));
         //assertThat(result, is(8L));
+    }
+}
+*/
+package mypackage;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CalculatorTest {
+    @Test
+    public void twoAndThreeIsFive() throws Exception {
+        final long result = new Calculator().addFucn(2, 3);
+        assertThat(result, is(5L));
+    }
+
+    @Test
+    public void threeMinusTwoIsOne() throws Exception {
+        final long result = new Calculator().subFunc(2, 3); // Corrected the method name
+        assertThat(result, is(1L));
+    }
+    
+    @Test
+    public void threeXThreeIsNine() throws Exception {
+        final long result = new Calculator().mulFucn(3, 3);
+        assertThat(result, is(9L));
     }
 }
